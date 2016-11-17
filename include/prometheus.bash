17c3f4535f647bd1dea804ad12c9593e062e2725
lalyos <lalyos@yahoo.com>
2016-11-17 10:23:29 +0100

    generate multiple targets

--- include/prometheus.bash
+++ include/prometheus.bash
@@ -22,7 +22,21 @@ get-cluster-ips() {
 
    curl -s -H "Authorization: Bearer $TOKEN"  -k https://$PUBLIC_IP/cb/api/v1/stacks/user/$cluster \
   | jq '{"PublicIP": .instanceGroups[].metadata[].publicIp}' \
-  | jq -s .
+  | jq -s -c .
+}
+
+prometheus-generate-targets-yaml() {
+    declare desc="Genberates tagets yml, and loads it into consul"
+    declare clusterIpsJson=${1:?required} port=${2:?required} path=${3:?required}
+    
+    local clusterIpsYml=$(
+      sigil -i '- targets:{{- range $k,$v := stdin | json  }}{{"\n"}}  - {{ $v.PublicIP  }}:{{$PORT}}{{- end }}' PORT=$port <<< "$clusterIpsJson"
+    )
+    debug "clusterIpsYml=$clusterIpsYml"
+
+    curl $PUBLIC_IP:8500/v1/kv/${path} -XPUT -d "$clusterIpsYml"
+
+
 }
 prometheus-monitor-cluster() {
     declare desc="Starts a cluster monitoring with prometheus"
@@ -36,12 +50,9 @@ prometheus-monitor-cluster() {
     local clusterIpsJson=$(get-cluster-ips $cluster)
     debug "clusterIpsJson: $clusterIpsJson"
     
-    local clusterIpsYml=$(
-      sigil -i '- targets:{{- range $k,$v := stdin | json  }}{{"\n"}}  - {{ $v.PublicIP  }}:9100{{- end }}' <<< "$clusterIpsJson"
-    )
-    debug "clusterIpsYml=$clusterIpsYml"
-
-    curl $PUBLIC_IP:8500/v1/kv/etc/prometheus/sdiscovery/node_collector.yml -XPUT -d "$clusterIpsYml"
+    prometheus-generate-targets-yaml "$clusterIpsJson" 9100 etc/prometheus/sdiscovery/node_collector.yml
+    prometheus-generate-targets-yaml "$clusterIpsJson" 20101 etc/prometheus/sdiscovery/jmx_resourcemanager.yml
+    prometheus-generate-targets-yaml "$clusterIpsJson" 20103 etc/prometheus/sdiscovery/jmx_namenode.yml
 
 
 }
