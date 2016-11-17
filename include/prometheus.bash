f71949ce78c3aaa8c94e3a326375122bd6df049d
lalyos <lalyos@yahoo.com>
2016-11-17 15:22:41 +0100

    add jmx generator

--- include/prometheus.bash
+++ include/prometheus.bash
@@ -38,6 +38,20 @@ prometheus-generate-targets-yaml() {
 
 
 }
+
+get-cluster-master_ip() {
+  declare cluster=${1:? required clusterName}
+   curl -s -H "Authorization: Bearer $TOKEN"  -k https://$PUBLIC_IP/cb/api/v1/stacks/user/$cluster \
+  | jq '{"PublicIP": .instanceGroups[].metadata[] | select(.instanceGroup=="master").publicIp}' \
+  | jq -s -c .
+}
+get-cluster-slave_ip() {
+  declare cluster=${1:? required clusterName}
+   curl -s -H "Authorization: Bearer $TOKEN"  -k https://$PUBLIC_IP/cb/api/v1/stacks/user/$cluster \
+  | jq '{"PublicIP": .instanceGroups[].metadata[] | select(.instanceGroup=="slave_1").publicIp}' \
+  | jq -s -c .
+}
+
 prometheus-monitor-cluster() {
     declare desc="Starts a cluster monitoring with prometheus"
 
@@ -50,9 +64,15 @@ prometheus-monitor-cluster() {
     local clusterIpsJson=$(get-cluster-ips $cluster)
     debug "clusterIpsJson: $clusterIpsJson"
 
+    local clusterMasterIpsJson=$(get-cluster-master_ip $cluster)
+    debug "clusterMasterIpsJson: $clusterMasterIpsJson"
+    local clusterSlaveIpsJson=$(get-cluster-slave_ip $cluster)
+    debug "clusterSlaveIpsJson: $clusterSlaveIpsJson"
     prometheus-generate-targets-yaml "$clusterIpsJson" 9100 etc/prometheus/sdiscovery/node_collector.yml
-    prometheus-generate-targets-yaml "$clusterIpsJson" 20101 etc/prometheus/sdiscovery/jmx_resourcemanager.yml
-    prometheus-generate-targets-yaml "$clusterIpsJson" 20103 etc/prometheus/sdiscovery/jmx_namenode.yml
+    prometheus-generate-targets-yaml "$clusterMasterIpsJson" 20101 etc/prometheus/sdiscovery/jmx_resourcemanager.yml
+    prometheus-generate-targets-yaml "$clusterSlaveIpsJson" 20102 etc/prometheus/sdiscovery/jmx_nodemanager.yml
+    prometheus-generate-targets-yaml "$clusterMasterIpsJson" 20103 etc/prometheus/sdiscovery/jmx_namenode.yml
+
     prometheus-generate-targets-yaml "$clusterIpsJson" 9246 etc/prometheus/sdiscovery/process.yml
 
 
