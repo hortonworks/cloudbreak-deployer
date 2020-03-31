ee71d7208e710b6b82cbf896c28c8cc6f5be7935
Peter Erdos <perdos@cloudera.com>
2020-03-31 08:05:52 +0200

    add datalake-api to cbd

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -50,8 +50,9 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_FREEIPA 2.19.0-rc.111
     env-import DOCKER_TAG_ULUWATU 2.19.0-rc.111
 
-    env-import DOCKER_TAG_IDBMMS 76df5355c0f5776c2288a38f731cf46c30e9c146
-    env-import DOCKER_TAG_ENVIRONMENTS2_API d4f8f7e4106895e3dec7bfd354e4aefb087823c6
+    env-import DOCKER_TAG_IDBMMS 17fd200ed122134be60a89a6d331102dfa1f1a36
+    env-import DOCKER_TAG_ENVIRONMENTS2_API 17fd200ed122134be60a89a6d331102dfa1f1a36
+    env-import DOCKER_TAG_DATALAKE_API 17fd200ed122134be60a89a6d331102dfa1f1a36
 
     env-import DOCKER_TAG_POSTGRES 9.6.16-alpine
     env-import DOCKER_TAG_LOGROTATE 1.0.2
@@ -71,6 +72,7 @@ cloudbreak-conf-tags() {
 
     env-import DOCKER_IMAGE_IDBMMS docker-private.infra.cloudera.com/cloudera/thunderhead-idbrokermappingmanagement
     env-import DOCKER_IMAGE_ENVIRONMENTS2_API docker-private.infra.cloudera.com/cloudera/thunderhead-environments2-api
+    env-import DOCKER_IMAGE_DATALAKE_API docker-private.infra.cloudera.com/cloudera/thunderhead-datalake-api
     env-import DOCKER_IMAGE_CLUSTER_PROXY docker-private.infra.cloudera.com/cloudera/dps-cluster-proxy
 
     env-import CB_DEFAULT_SUBSCRIPTION_ADDRESS http://uluwatu:3000/notifications
@@ -142,6 +144,7 @@ cloudbreak-conf-db() {
     env-import CLUSTER_PROXY_DB_ENV_DB "cluster_proxy"
     env-import CLUSTER_PROXY_DB_ENV_USER "postgres"
     env-import CLUSTER_PROXY_DB_ENV_PASS ""
+    env-import DATALAKE_API_TARGET_PATH
 
     env-import VAULT_DB_SCHEMA "vault"
 }
@@ -220,6 +223,7 @@ cloudbreak-conf-defaults() {
     env-import IDBMMS_URL $(service-url idbmms "$BRIDGE_ADDRESS" "$CB_LOCAL_DEV_LIST" "" "8990" "8982")
     env-import IDBMMS_HOST $(host-from-url "$IDBMMS_URL")
     env-import ENVIRONMENTS2_API_URL $(service-url environments2-api "$BRIDGE_ADDRESS" "$CB_LOCAL_DEV_LIST" "http://" "8984" "8982")
+    env-import DATALAKE_API_URL $(service-url datalake-api "$BRIDGE_ADDRESS" "$CB_LOCAL_DEV_LIST" "http://" "8986" "8984")
 
     env-import ENVIRONMENT_PORT $(port-from-url "$ENVIRONMENT_URL")
     env-import FREEIPA_PORT $(port-from-url "$FREEIPA_URL")
@@ -232,6 +236,7 @@ cloudbreak-conf-defaults() {
     env-import IDBMMS_PORT $(port-from-url "$IDBMMS_URL")
     env-import IDBMMS_HEALTHZ_PORT 8991
     env-import ENVIRONMENTS2_API_HEALTHZ_PORT 8983
+    env-import DATALAKE_API_HEALTHZ_PORT 8985
 
     env-import UAA_ULUWATU_SECRET "dummysecret"
 
@@ -381,7 +386,7 @@ generate-toml-file-for-localdev() {
 
 generate-toml-file-for-localdev-force() {
     declare traefikFile=${1:-traefik.toml}
-    generate-traefik-toml "$CLOUDBREAK_URL" "$PERISCOPE_URL" "$DATALAKE_URL" "$ENVIRONMENT_URL" "$REDBEAMS_URL" "$FREEIPA_URL" "http://$CAAS_URL" "$CLUSTER_PROXY_URL" "$ENVIRONMENTS2_API_URL" "$CB_LOCAL_DEV_LIST" > "$traefikFile"
+    generate-traefik-toml "$CLOUDBREAK_URL" "$PERISCOPE_URL" "$DATALAKE_URL" "$ENVIRONMENT_URL" "$REDBEAMS_URL" "$FREEIPA_URL" "http://$CAAS_URL" "$CLUSTER_PROXY_URL" "$ENVIRONMENTS2_API_URL" "$DATALAKE_API_URL" "$CB_LOCAL_DEV_LIST" > "$traefikFile"
 }
 
 generate-traefik-check-diff() {
--- traefik.go
+++ traefik.go
@@ -18,11 +18,12 @@ type traefikTomlParams struct {
 	CaasURL             string
 	ClusterProxyURL     string
 	Environments2ApiURL string
+	DatalakeApiURL 		string
 	LocalDevList        string
 }
 
 func GenerateTraefikToml(args []string) {
-	params := traefikTomlParams{args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]}
+	params := traefikTomlParams{args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10]}
 	if len(params.LocalDevList) == 0 {
 		fmt.Print("")
 	} else {
