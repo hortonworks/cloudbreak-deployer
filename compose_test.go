20861c7b9ec8fe1915ec9c8b25203601f05856dc
Richard Doktorics <rdoktorics@cloudera.com>
2020-06-30 22:20:02 +0200

    Revert "Revert "caas rename""

--- compose_test.go
+++ compose_test.go
@@ -7,10 +7,10 @@ import (
 
 var inputVars = `
 export CB_LOCAL_DEV_LIST=cloudbreak
-export CAAS_MOCK_BIND_PORT=10080
-export CAAS_MOCK_VOLUME_CONTAINER=/tmp/null
-export CAAS_MOCK_VOLUME_HOST=/dev/null
-export CAAS_URL=caas-api:8080
+export THUNDERHEAD_MOCK_BIND_PORT=10080
+export THUNDERHEAD_MOCK_VOLUME_CONTAINER=/tmp/null
+export THUNDERHEAD_MOCK_VOLUME_HOST=/dev/null
+export THUNDERHEAD_URL=thunderhead-api:8080
 export CBD_CERT_ROOT_PATH=/Users/bbihari/prj/cbd-local/certs
 export CBD_TRAEFIK_TLS=/certs/traefik/client.pem,/certs/traefik/client-key.pem
 export CB_INSTANCE_NODE_ID=8a93ca4a-2004-4a41-9565-257060982457
@@ -22,7 +22,7 @@ export CB_SHOW_TERMINATED_CLUSTERS_MINUTES=0
 export CLOUDBREAK_URL=http://host.docker.internal:9091
 export COMMON_DB=commondb
 export COMMON_DB_VOL=common
-export DOCKER_IMAGE_CAAS_MOCK=hortonworks/cloudbreak-mock-caas
+export DOCKER_IMAGE_THUNDERHEAD_MOCK=hortonworks/cloudbreak-mock-thunderhead
 export DOCKER_IMAGE_CLOUDBREAK=docker-private.infra.cloudera.com/cloudera/cloudbreak
 export DOCKER_IMAGE_CLOUDBREAK_DATALAKE=docker-private.infra.cloudera.com/cloudera/cloudbreak-datalake
 export DOCKER_IMAGE_CLOUDBREAK_ENVIRONMENT=docker-private.infra.cloudera.com/cloudera/cloudbreak-environment
@@ -31,7 +31,7 @@ export DOCKER_IMAGE_CLOUDBREAK_REDBEAMS=docker-private.infra.cloudera.com/cloude
 export DOCKER_IMAGE_CLOUDBREAK_FREEIPA=docker-private.infra.cloudera.com/cloudera/cloudbreak-freeipa
 export DOCKER_IMAGE_CLOUDBREAK_WEB=docker-private.infra.cloudera.com/cloudera/hdc-web
 export DOCKER_NETWORK_NAME=default
-export DOCKER_TAG_CAAS_MOCK=2.10.0-dev.669
+export DOCKER_TAG_THUNDERHEAD_MOCK=2.10.0-dev.669
 export DOCKER_TAG_CLOUDBREAK=2.10.0-dev.669
 export DOCKER_TAG_DATALAKE=2.10.0-dev.669
 export DOCKER_TAG_ENVIRONMENT=2.10.0-dev.805
@@ -88,7 +88,7 @@ func TestComposeGenerationWithDps(t *testing.T) {
 	out := catchStdInStdOut(t, inputVars+dpsVar, func() {
 		GenerateComposeYaml([]string{})
 	})
-	should := []string{`(?m)^\s*periscope:`, `(?m)^\s*datalake:`, `(?m)^\s*redbeams:`, `(?m)^\s*caas-api:`, `(?m)^\s*cluster-proxy:`, `(?m)^\s*core-gateway:`}
+	should := []string{`(?m)^\s*periscope:`, `(?m)^\s*datalake:`, `(?m)^\s*redbeams:`, `(?m)^\s*cluster-proxy:`}
 	shouldnt := []string{`(?m)^\s*cloudbreak:`}
 	for _, s := range should {
 		re := regexp.MustCompile(s)
