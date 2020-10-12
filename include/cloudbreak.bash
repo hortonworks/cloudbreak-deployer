c2b0a5b37bd40f9904f21089558cba1f0deb0c6d
Mészáros Gergely <grigorijat9026@gmail.com>
2020-10-12 15:52:41 +0200

    CB-9229 - ability to create idbroker mapping creation CDP CLI command for a non-existing environment

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -60,10 +60,10 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_FREEIPA 2.32.0-b88
     env-import DOCKER_TAG_ULUWATU 2.32.0-b88
 
-    env-import DOCKER_TAG_IDBMMS 1.0.0-b2193
-    env-import DOCKER_TAG_ENVIRONMENTS2_API 1.0.0-b2193
-    env-import DOCKER_TAG_DATALAKE_API 1.0.0-b2193
-    env-import DOCKER_TAG_DISTROX_API 1.0.0-b2193
+    env-import DOCKER_TAG_IDBMMS 1.0.0-b2235
+    env-import DOCKER_TAG_ENVIRONMENTS2_API 1.0.0-b2235
+    env-import DOCKER_TAG_DATALAKE_API 1.0.0-b2235
+    env-import DOCKER_TAG_DISTROX_API 1.0.0-b2235
 
     env-import DOCKER_TAG_POSTGRES 9.6.16-alpine
     env-import DOCKER_TAG_CBD_SMARTSENSE 0.13.4
