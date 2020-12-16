24b6862587ecee1fe1f7fe70ea7de0670253bdc0
Dénes Berger <dberger@cloudbreak.com>
2020-12-16 15:32:18 +0100

    CB-10371 bumped public api version that contains CLI-related updates
    Supporting getIdBrokerMappingsCliForEnv and missed swagger enum generation
    for SsoTypeEnum in GatewayV4Request

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -69,11 +69,11 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_FREEIPA 2.36.0-b65
     env-import DOCKER_TAG_ULUWATU 2.36.0-b65
 
-    env-import DOCKER_TAG_IDBMMS 1.0.0-b2601
-    env-import DOCKER_TAG_ENVIRONMENTS2_API 1.0.0-b2601
-    env-import DOCKER_TAG_DATALAKE_API 1.0.0-b2601
-    env-import DOCKER_TAG_DISTROX_API 1.0.0-b2601
-    env-import DOCKER_TAG_AUDIT 1.0.0-b2601
+    env-import DOCKER_TAG_IDBMMS 1.0.0-b2821
+    env-import DOCKER_TAG_ENVIRONMENTS2_API 1.0.0-b2821
+    env-import DOCKER_TAG_DATALAKE_API 1.0.0-b2821
+    env-import DOCKER_TAG_DISTROX_API 1.0.0-b2821
+    env-import DOCKER_TAG_AUDIT 1.0.0-b2821
 
     env-import DOCKER_TAG_POSTGRES 9.6.16-alpine
     env-import DOCKER_TAG_CBD_SMARTSENSE 0.13.4
