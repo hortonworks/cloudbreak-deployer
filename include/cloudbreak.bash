a02497eba72f8250ec678409f971b09c4ae4bba5
Lajos Rodek <15786876+lajosrodek@users.noreply.github.com>
2021-03-17 15:45:07 +0100

    CB-11544 Updated CDPCP container versions to 1.0.0-b3470
    Update all Thunderhead dependencies to version 1.0.0-b3470.

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -69,12 +69,12 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_FREEIPA 2.40.0-b108
     env-import DOCKER_TAG_ULUWATU 2.40.0-b108
 
-    env-import DOCKER_TAG_IDBMMS 1.0.0-b3063
-    env-import DOCKER_TAG_ENVIRONMENTS2_API 1.0.0-b3063
-    env-import DOCKER_TAG_DATALAKE_API 1.0.0-b3063
-    env-import DOCKER_TAG_DISTROX_API 1.0.0-b3063
-    env-import DOCKER_TAG_AUDIT 1.0.0-b3193
-    env-import DOCKER_TAG_DATALAKE_DR 1.0.0-b3073
+    env-import DOCKER_TAG_IDBMMS 1.0.0-b3470
+    env-import DOCKER_TAG_ENVIRONMENTS2_API 1.0.0-b3470
+    env-import DOCKER_TAG_DATALAKE_API 1.0.0-b3470
+    env-import DOCKER_TAG_DISTROX_API 1.0.0-b3470
+    env-import DOCKER_TAG_AUDIT 1.0.0-b3470
+    env-import DOCKER_TAG_DATALAKE_DR 1.0.0-b3470
 
     env-import DOCKER_TAG_POSTGRES 9.6.16-alpine
     env-import DOCKER_TAG_CBD_SMARTSENSE 0.13.4
