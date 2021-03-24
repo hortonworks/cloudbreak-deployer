3c3965352b9fd9668f940c87d64476d29f25f939
Richard Doktorics <rdoktorics@cloudera.com>
2021-03-24 18:05:05 +0100

    Update audit api to 1.0.0-b3557

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -73,7 +73,7 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_ENVIRONMENTS2_API 1.0.0-b3518
     env-import DOCKER_TAG_DATALAKE_API 1.0.0-b3518
     env-import DOCKER_TAG_DISTROX_API 1.0.0-b3518
-    env-import DOCKER_TAG_AUDIT 1.0.0-b3518
+    env-import DOCKER_TAG_AUDIT 1.0.0-b3557
     env-import DOCKER_TAG_DATALAKE_DR 1.0.0-b3518
 
     env-import DOCKER_TAG_POSTGRES 9.6.16-alpine
