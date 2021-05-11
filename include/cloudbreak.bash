229a02d6c164c4151ecda919c846c0bf48b38c32
jenkins <jenkins@hortonworks.com>
2021-05-11 05:55:44 +0000

    Updated container versions to 2.43.0-b74

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -59,15 +59,15 @@ cloudbreak-conf-tags() {
     env-import DOCKER_TAG_AMBASSADOR 0.5.0
     env-import DOCKER_TAG_CERT_TOOL 0.2.0
 
-    env-import DOCKER_TAG_THUNDERHEAD_MOCK 2.43.0-b73
-    env-import DOCKER_TAG_MOCK_INFRASTRUCTURE 2.43.0-b73
-    env-import DOCKER_TAG_PERISCOPE 2.43.0-b73
-    env-import DOCKER_TAG_CLOUDBREAK 2.43.0-b73
-    env-import DOCKER_TAG_DATALAKE 2.43.0-b73
-    env-import DOCKER_TAG_REDBEAMS 2.43.0-b73
-    env-import DOCKER_TAG_ENVIRONMENT 2.43.0-b73
-    env-import DOCKER_TAG_FREEIPA 2.43.0-b73
-    env-import DOCKER_TAG_ULUWATU 2.43.0-b73
+    env-import DOCKER_TAG_THUNDERHEAD_MOCK 2.43.0-b74
+    env-import DOCKER_TAG_MOCK_INFRASTRUCTURE 2.43.0-b74
+    env-import DOCKER_TAG_PERISCOPE 2.43.0-b74
+    env-import DOCKER_TAG_CLOUDBREAK 2.43.0-b74
+    env-import DOCKER_TAG_DATALAKE 2.43.0-b74
+    env-import DOCKER_TAG_REDBEAMS 2.43.0-b74
+    env-import DOCKER_TAG_ENVIRONMENT 2.43.0-b74
+    env-import DOCKER_TAG_FREEIPA 2.43.0-b74
+    env-import DOCKER_TAG_ULUWATU 2.43.0-b74
 
     env-import DOCKER_TAG_IDBMMS 1.0.0-b3944
     env-import DOCKER_TAG_ENVIRONMENTS2_API 1.0.0-b3944
