85f06e7b39a4f50a1eb14b3a7508e68e586d20a7
attilapalfi92 <apalfi@cloudera.com>
2021-05-19 12:51:22 +0200

    CB-9310 - cluster proxy version bump

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -79,8 +79,8 @@ cloudbreak-conf-tags() {
 
     env-import DOCKER_TAG_POSTGRES 13.2-alpine
     env-import DOCKER_TAG_CBD_SMARTSENSE 0.13.4
-    env-import DOCKER_TAG_CLUSTER_PROXY 1.0.0-b40
-    env-import DOCKER_TAG_CLUSTER_PROXY_HEALTH_CHECK_WORKER 1.0.0-b40
+    env-import DOCKER_TAG_CLUSTER_PROXY 1.0.0-b100
+    env-import DOCKER_TAG_CLUSTER_PROXY_HEALTH_CHECK_WORKER 1.0.0-b100
     env-import DOCKER_TAG_CADENCE 0.11.0-auto-setup
     env-import DOCKER_TAG_CADENCE_WEB 1.0.0-b24
 
