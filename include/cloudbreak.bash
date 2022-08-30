a0f0a6c824d54586c62020f71bb1dbcee13c0d4c
Dénes Berger <dberger@cloudera.com>
2022-08-30 15:42:25 +0200

    Upgrade Cluster Proxy to 1.0.9-b9
    Communication with CCMv1 service was not working anymore
    with 1.0.8-b6

--- include/cloudbreak.bash
+++ include/cloudbreak.bash
@@ -82,8 +82,8 @@ cloudbreak-conf-tags() {
 
     env-import DOCKER_TAG_POSTGRES 13.2-alpine
     env-import DOCKER_TAG_CBD_SMARTSENSE 0.13.4
-    env-import DOCKER_TAG_CLUSTER_PROXY 1.0.8-b6
-    env-import DOCKER_TAG_CLUSTER_PROXY_HEALTH_CHECK_WORKER 1.0.8-b6
+    env-import DOCKER_TAG_CLUSTER_PROXY 1.0.9-b9
+    env-import DOCKER_TAG_CLUSTER_PROXY_HEALTH_CHECK_WORKER 1.0.9-b9
     env-import DOCKER_TAG_CADENCE 0.11.0-auto-setup
     env-import DOCKER_TAG_CADENCE_WEB 1.0.0-b24
 
